97da6cdf92c7aa93ed3deb85a4958fc792857113
Pramod Jayathilaka <pramodjayathilaka@gmail.com>
2024-11-01 22:46:35 +0530

    fixes

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <div className="w-full h-16 bg-gray opacity-70 absolute">
         <div className="flex w-32 h-full items-center justify-center">
-            <Image src="/images/logo.png" alt="marisha logo" className="w-24"/>
+            <Image width={50} height={50} src="/images/logo.png" alt="marisha logo" className="hidden sm:block w-24"/>
         </div>
         <div className="flex items-center justify-center">
         
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
 const geistSans = localFont({
